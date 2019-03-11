79908d62044674b149974826b5b8e8e4895da69a
siddhartha <Phantom1650763>
2019-03-11 19:54:09 +0530

    Initial commit

---
+++ app/src/main/java/in/creativelizard/picx/MainActivity.kt
@@ -0,0 +1,43 @@
+package `in`.creativelizard.picx
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import android.view.MotionEvent
+
+
+
+
+
+
+class MainActivity : AppCompatActivity() {
+    var dX: Float = 0.toFloat()
+    var dY:Float = 0.toFloat()
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        rbPanel.setOnTouchListener { view, event ->
+            when (event.action) {
+
+                MotionEvent.ACTION_DOWN -> {
+
+                    dX = view.x - event.rawX
+                    dY = view.y - event.rawY
+                }
+
+                MotionEvent.ACTION_MOVE ->
+
+                    view.animate()
+                        .x(event.rawX + dX)
+                        .y(event.rawY + dY)
+                        .setDuration(0)
+                        .start()
+                else -> return@setOnTouchListener false
+            }
+            return@setOnTouchListener true
+        }
+    }
+
+
+}
